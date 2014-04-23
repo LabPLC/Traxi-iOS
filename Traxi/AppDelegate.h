d2eb41cfc440c420f3c6b685b26d222480a80f65
Carlos Castellanos <rockarloz@me.com>
2014-04-23 11:09:29 -0500

    Initial Commit

---
+++ Traxi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Traxi
+//
+//  Created by Carlos Castellanos on 23/04/14.
+//  Copyright (c) 2014 Carlos Castellanos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
